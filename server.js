c143c2ff70518aa1b4f84307b0003ba390245371
Victor Tolbert <victor.tolbert@gmail.com>
2023-12-02 02:48:09 -0500

    fix: http-auth

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const process = require('node:process')
 const express = require('express')
 
-const app = express()
+const auth = require('http-auth')
 
 const port = process.env.PORT || 3000
+
+const basic = auth.basic({
+  realm: 'Salesforce Lightning Design System Prototype',
+}, (username, password, next) => {
+  next(username === process.env.USERNAME && password === process.env.PASSWORD)
+})
+
+if (process.env.USERNAME && process.env.PASSWORD)
+  app.use(auth.connect(basic))
 
 app.use('/', express.static('./dist'))
 
