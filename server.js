bb80720c03edd434e64c2f2d409ba5db461fb62f
Victor Tolbert <victor.tolbert@gmail.com>
2023-12-02 03:14:06 -0500

    chore: remove basic auth

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const process = require('node:process')
 const express = require('express')
-const auth = require('http-auth')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const basic = auth.basic({
-  realm: 'Starter Kit',
-}, (username, password, callback) => {
-  callback(username === process.env.USERNAME && password === process.env.PASSWORD)
-})
-
-// Use the basic auth middleware directly
-if (process.env.USERNAME && process.env.PASSWORD) {
-  app.use((req, res, next) => {
-    basic.check(req, res, next)
-  })
-}
 
 app.use('/', express.static('./dist'))
 
