5d2cc6a14be88d21ee1ccf79c0d3fc3bdc24d44f
Victor Tolbert <victor.tolbert@gmail.com>
2024-01-25 19:17:05 -0500

    wip

--- src/scripts/salesforce.js
+++ src/scripts/salesforce.js
@@ -28,13 +28,13 @@ console.log('Code in app.js is not built for accessibility, performance, or cros
  * Open / Close an element
  * @param {Node} element
  */
-const toggle = (element) => element.classList.toggle('slds-is-open')
+const toggle = element => element.classList.toggle('slds-is-open')
 
 /**
  * Open / Close an element's parent
  * @param {Node} element
  */
-const toggleParent = (element) => toggle(element.parentNode)
+const toggleParent = element => toggle(element.parentNode)
 
 /**
  * Dropdown menus
@@ -43,8 +43,8 @@ const toggleParent = (element) => toggle(element.parentNode)
 const dropdownButtons = document.querySelectorAll('.slds-dropdown-trigger_click > .slds-button')
 
 Array.from(dropdownButtons)
-  .forEach((button) =>
-    button.addEventListener('click', (event) => toggleParent(event.currentTarget), false)
+  .forEach(button =>
+    button.addEventListener('click', event => toggleParent(event.currentTarget), false),
   )
 
 /**
@@ -52,17 +52,21 @@ Array.from(dropdownButtons)
  * https://www.lightningdesignsystem.com/components/tabs/
  */
 
-const tabs = (variant) => document.querySelectorAll(`.slds-tabs_${variant} [role=tablist] [role=tab]`)
-const tabActiveReset = (tab) => Array.from(tab.parentNode.parentNode.querySelectorAll('li'))
-  .forEach((element) => element.classList.remove('slds-active'))
-const tabActiveSet = (tab) => tab.parentNode.classList.add('slds-active')
-const tabPanelsReset = (tab) => Array.from(tab.parentNode.parentNode.parentNode.querySelectorAll('[role="tabpanel"]'))
-  .forEach((tabpanel) => {
-    tabpanel.classList.remove('slds-show')
-    tabpanel.classList.remove('slds-hide')
-    tabpanel.classList.add('slds-hide')
-  })
-const tabPanelShow = (tab) => {
+const tabs = variant => document.querySelectorAll(`.slds-tabs_${variant} [role=tablist] [role=tab]`)
+function tabActiveReset(tab) {
+  return Array.from(tab.parentNode.parentNode.querySelectorAll('li'))
+    .forEach(element => element.classList.remove('slds-active'))
+}
+const tabActiveSet = tab => tab.parentNode.classList.add('slds-active')
+function tabPanelsReset(tab) {
+  return Array.from(tab.parentNode.parentNode.parentNode.querySelectorAll('[role="tabpanel"]'))
+    .forEach((tabpanel) => {
+      tabpanel.classList.remove('slds-show')
+      tabpanel.classList.remove('slds-hide')
+      tabpanel.classList.add('slds-hide')
+    })
+}
+function tabPanelShow(tab) {
   const tabpanel = document.getElementById(tab.getAttribute('aria-controls'))
   tabpanel.classList.remove('slds-show')
   tabpanel.classList.remove('slds-hide')
@@ -72,7 +76,7 @@ const tabPanelShow = (tab) => {
 const defaultTabs = tabs('default')
 const scopedTabs = tabs('scoped')
 
-const assignTabEvents = (event) => {
+function assignTabEvents(event) {
   tabActiveReset(event.currentTarget)
   tabActiveSet(event.currentTarget)
   tabPanelsReset(event.currentTarget)
